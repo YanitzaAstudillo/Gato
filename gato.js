6e7e9f0843320ba3f9f9750d9e1394fbcaa2e176
YanitzaAstudillo <astudilloyanitza@gmail.com>
2025-02-17 15:27:02 -0600

    CONTADOR

--- gato.js
+++ gato.js
@@ -1,5 +1,10 @@
 const cuadritos = Array.from(document.getElementsByClassName("cuadro"))
-const boton = document.getElementById("Reiniciar")
+const boton = document.getElementById("reiniciar")
+const button = document.getElementById("empezarJuego")
+const temporizador = document.getElementById("tiempo")
+let contadorX = 0
+let contadorO = 0
+
 
 //mover humanoX0 con click - identidad => del forEach - miliretraso a maquina -detecta gane//
 const turnos = ["X","O"];
@@ -10,15 +15,16 @@ function moverJugador() {
               cuadro.innerHTML = turnos[0];
               setTimeout(() => {
                   moverMaquina()
-              }, 1000);
+              }, 800)
               detectarGanar()
+              
             }
         });
     });
 }
 //mover maquinaO1 usando metodo filter y Math random para casilla aleatoria por tamaño completo//
 function moverMaquina() {
-    const cuadrosVacios= cuadritos.filter (cuadro => cuadro.innerHTML === "" );
+    const cuadrosVacios= cuadritos.filter (cuadro => cuadro.innerHTML === "");
     if (cuadrosVacios.length > 0) {
         const cuadroCualquiera = cuadrosVacios [Math.floor(Math.random() * cuadrosVacios.length)];
             cuadroCualquiera.innerHTML = turnos[1];
@@ -43,28 +49,46 @@ function detectarGanar() {
 for (const poss of ganadoras) {
     let [a,b,c] = poss
     if (cuadritos[a].textContent != "" && cuadritos[a].textContent === cuadritos[b].textContent && cuadritos[a].textContent === cuadritos[c].textContent) {
-        setTimeout(() => {
-        alert("GANÓ "+ cuadritos[a].textContent)
-        }, 1000);
+       if (cuadritos[a].textContent === "X") {
+           localStorage.setItem("contadorX",contadorX)
+            contadorX++
+            setTimeout (() =>{
+                alert("GANÓ X")
+            },1000)
+            return
+            
+       }else if(cuadritos[a].textContent === "O"){
+           localStorage.setItem("contadorO",contadorO)
+            contadorO++
+            setTimeout (() =>{
+                alert("GANÓ O")
+            },1000)
+            return
+       }
     }
 }
 };
-//almacenar estadisticas LocalStorage//
-let marcador = JSON.parse (localStorage.getItem("ganaHumano")) || []
-    if (marcador.length > 0) {
-        puntaje (marcador)
-    } 
-boton.addEventListener("click", function () {
-    marcador.push(cuadritos.value)
-    localStorage.setItem("ganaHumano", JSON.stringify(marcador))
-        puntaje(marcador)
-});
-let marcador2 = JSON.parse (localStorage.getItem ("ganaMaquina")) || []
-    if (marcador2.length > 1) {
-        puntaje(marcador2)
+//  cuenta regresiva con setInterval//
+let terminaCuenta =false
+let redflag =false
+
+function cuentaRegresiva(tiempo){
+    setInterval(() => {
+        if (tiempo>0) {
+            tiempo--
+            temporizador.textContent=tiempo
+        }
+        if (tiempo===0 && !redflag) {
+            alert ("GAME OVER")
+            redflag = true
+         }  
+    }, 1000);
 }
-boton.addEventListener("click", function () {
-    marcador2.push(cuadritos.value)
-    localStorage.setItem("ganaMaquina", JSON.stringify("ganaMaquina"))
-        puntaje(marcador2)
+// funcion button empezar juego //
+button.addEventListener ("click", function () {
+    cuentaRegresiva(8)
+    if (cuentaRegresiva === 0) {
+        window.location.reload();
+    }
 });
+
